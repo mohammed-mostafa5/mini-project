0b632626d536c7b7522464226f7859137bf942ab
mohammed-mostafa5 <m5z.mohammed@gmail.com>
2022-01-02 17:44:08 +0200

    Auth system

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+
+    public function register()
+    {
+        $validated = request()->validate([
+            'name'      => 'required|string|min:3|max:191',
+            'email'     => 'required|email|unique:users,email',
+            'password'  => 'required|min:3|max:191|confirmed'
+        ]);
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'message' => 'User registered Successfully',
+            'user'    => $user
+        ]);
+    }
+
     public function login()
     {
         $credentials = request()->validate([
@@ -20,6 +38,13 @@ class AuthController extends Controller
 
         $token = request()->user()->createToken('token');
 
-        return response()->json(['token' => $token]);
+        return response()->json(['token' => $token->plainTextToken]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json(['message' => 'You logged out Successfully']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('test', function () {
+    return 'ok';
+});
+
+//////////////////////////////// Start Auth //////////////////////////////////
+Route::post('user/register', [ AuthController::class,'register']);
+Route::post('user/login', [ AuthController::class,'login']);
+//////////////////////////////// End Auth //////////////////////////////////
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('user')->middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [ AuthController::class,'logout']);
+});
+
+Route::prefix('admin')->middleware(['auth:sanctum', 'admin'])->group(function () {
+
+
 });
